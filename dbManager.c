300ad19a8c5fb1cc18ea849d466a1d9cebd706e0
ziadshebl <ziadshebl@gmail.com>
2020-05-27 10:52:50 -0700

    Sending the modify message + adding functions

--- dbManager.c
+++ dbManager.c
@@ -22,7 +22,7 @@ int main(int argc, char*argv[]){
     int sharedMemoryId = atoi(argv[1]);
     int ManagerClientMessageQid = atoi(argv[2]);
     int messageRecieveStatus;
-    struct record * memoryLocationStart = (struct record *) shmat(sharedMemoryId,(void*)0,0); 
+    //struct record * memoryLocationStart = (struct record *) shmat(sharedMemoryId,(void*)0,0); 
      printf("I am the dbManager\n");
      printf("The shared memory id is: %d \n",sharedMemoryId);
     // printf("The message Q id is: %d \n",ManagerClientMessageQid);
@@ -40,9 +40,9 @@ int main(int argc, char*argv[]){
                 printf("After strcpy... \n");
                 record.salary=message.operationMessage.addBuffer.salary;
                 //printf("The size of struct is: %ld \n",sizeof(struct record));
-                printf("The memory location is: %x \n",memoryLocationStart);
-                *(memoryLocationStart+(28*key))=record;
-                printf("After adding in shared mem... \n");
+                //printf("The memory location is: %x \n",memoryLocationStart);
+                //*(memoryLocationStart+(28*key))=record;
+                //printf("After adding in shared mem... \n");
                 onAdditionSuccess.key=key;
                 onAdditionSuccess.mtype=message.operationMessage.addBuffer.clientPID;
                 printf("Before sending....\n");
@@ -53,8 +53,8 @@ int main(int argc, char*argv[]){
                     key++; // to be handled 
                 }
 
-                struct record secondEntry=*(memoryLocationStart+ sizeof(struct record)*(key-1));
-                printf("From the shared memory the name added is: %s \n",secondEntry.name);
+               //struct record secondEntry=*(memoryLocationStart+ sizeof(struct record)*(key-1));
+                //printf("From the shared memory the name added is: %s \n",secondEntry.name);
             }
             else if(message.operationMessage.operationNeeded==modify)
             {
